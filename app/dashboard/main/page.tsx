fdcc998aec75670b89647b49f95811dd1d024f91
sauderx <ronaldpradopalacios@gmail.com>
2026-07-13 19:02:40 -0400

    Uso de STORE global: comunicar component MAIN con COUNTER

--- app/dashboard/main/page.tsx
+++ app/dashboard/main/page.tsx
@@ -1,4 +1,4 @@
-import { SimpleWidget } from "@/app/components/SimpleWidget";
+import { WidgetsGrid } from "@/app/components/dashboard/WidgetsGrid";
 
 export const metadata = {
   title: "Admin Dashboard",
@@ -11,9 +11,7 @@ export default function MainPage() {
       <h1 className="mt-2 text-3xl">Dashboard</h1>
       <span className="text-xl">Información general</span>
 
-      <div className="flex flex-wrap p-2 items-center justify-center">
-        <SimpleWidget />
-      </div>
+      <WidgetsGrid />
     </div>
   );
 }
